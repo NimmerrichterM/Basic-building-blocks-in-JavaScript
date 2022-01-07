eed75d322e503750bbebe87912ad3da9b5d1bb7a
NimmerrichterM <60734264+NimmerrichterM@users.noreply.github.com>
2022-01-07 18:40:36 +0100

    Done

---
+++ script.js
@@ -0,0 +1,40 @@
+let nine = 10
+for (let i = 1; i <= nine; i++) {
+    console.log(i)
+}
+
+//printNumbersTill(20)
+
+
+
+let printNumbersTill = (x) => {
+    for (let i = 1; i <= a; i++) ;
+     }
+      console.log(15);
+
+
+
+      function getGreetingTo(name) {
+        console.log("Hello, " + name + "!");
+      }
+      
+      getGreetingTo("Mark");
+
+      let printValues = (array) => {
+        for (let i = 0; i < array.length; i++) {
+          console.log(array[i]);
+        }
+      };
+      printValues([1,33,4,5,6,])
+
+      let myAray = ([34,43,89,100]); 
+          myAray.forEach(element =>console.log(element));
+          
+        
+
+          
+      
+
+
+      
+      
